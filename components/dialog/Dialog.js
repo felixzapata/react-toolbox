5fb3b30f2209a9d03eaa5c985a1f502241f92f68
Felix Zapata <fzberlinches@gmail.com>
2016-08-24 09:21:03 +0200

    feat(Dialog.js): create a function to inject the components for Dialog

--- components/dialog/Dialog.js
+++ components/dialog/Dialog.js
@@ -5,72 +5,81 @@ import { DIALOG } from '../identifiers.js';
 import Button from '../button/Button.js';
 import Overlay from '../overlay/Overlay.js';
 
-class Dialog extends Component {
-  static propTypes = {
-    actions: PropTypes.array,
-    active: PropTypes.bool,
-    children: PropTypes.node,
-    className: PropTypes.string,
-    onEscKeyDown: PropTypes.func,
-    onOverlayClick: PropTypes.func,
-    onOverlayMouseDown: PropTypes.func,
-    onOverlayMouseMove: PropTypes.func,
-    onOverlayMouseUp: PropTypes.func,
-    theme: PropTypes.shape({
-      active: PropTypes.string,
-      body: PropTypes.string,
-      button: PropTypes.string,
-      dialog: PropTypes.string,
-      navigation: PropTypes.string,
-      title: PropTypes.string
-    }),
-    title: PropTypes.string,
-    type: PropTypes.string
-  };
 
-  static defaultProps = {
-    actions: [],
-    active: false,
-    type: 'normal'
-  };
+var factory = function factory(Overlay, Button) {
 
-  render () {
-    const actions = this.props.actions.map((action, idx) => {
-      const className = classnames(this.props.theme.button, {[action.className]: action.className});
-      return <Button key={idx} {...action} className={className} />;
-    });
+  class Dialog extends Component {
+    static propTypes = {
+      actions: PropTypes.array,
+      active: PropTypes.bool,
+      children: PropTypes.node,
+      className: PropTypes.string,
+      onEscKeyDown: PropTypes.func,
+      onOverlayClick: PropTypes.func,
+      onOverlayMouseDown: PropTypes.func,
+      onOverlayMouseMove: PropTypes.func,
+      onOverlayMouseUp: PropTypes.func,
+      theme: PropTypes.shape({
+        active: PropTypes.string,
+        body: PropTypes.string,
+        button: PropTypes.string,
+        dialog: PropTypes.string,
+        navigation: PropTypes.string,
+        title: PropTypes.string
+      }),
+      title: PropTypes.string,
+      type: PropTypes.string
+    };
 
-    const className = classnames([this.props.theme.dialog, this.props.theme[this.props.type]], {
-      [this.props.theme.active]: this.props.active
-    }, this.props.className);
+    static defaultProps = {
+      actions: [],
+      active: false,
+      type: 'normal'
+    };
 
-    return (
-      <Overlay
-        active={this.props.active}
-        onClick={this.props.onOverlayClick}
-        onEscKeyDown={this.props.onEscKeyDown}
-        onMouseDown={this.props.onOverlayMouseDown}
-        onMouseMove={this.props.onOverlayMouseMove}
-        onMouseUp={this.props.onOverlayMouseUp}
-      >
-        <div data-react-toolbox='dialog' role='dialog' tabindex='-1' className={className}>
-          <section role='body' className={this.props.theme.body}>
-            {this.props.title ? <h6 className={this.props.theme.title}>{this.props.title}</h6> : null}
-            {this.props.children}
-          </section>
-          {actions.length
-            ? <nav role='navigation' className={this.props.theme.navigation}>
-                {actions}
-              </nav>
-            : null
-          }
-        </div>
-      </Overlay>
-    );
+    render () {
+      const actions = this.props.actions.map((action, idx) => {
+        const className = classnames(this.props.theme.button, {[action.className]: action.className});
+        return <Button key={idx} {...action} className={className} />;
+      });
+
+      const className = classnames([this.props.theme.dialog, this.props.theme[this.props.type]], {
+        [this.props.theme.active]: this.props.active
+      }, this.props.className);
+
+      return (
+        <Overlay
+          active={this.props.active}
+          onClick={this.props.onOverlayClick}
+          onEscKeyDown={this.props.onEscKeyDown}
+          onMouseDown={this.props.onOverlayMouseDown}
+          onMouseMove={this.props.onOverlayMouseMove}
+          onMouseUp={this.props.onOverlayMouseUp}
+        >
+          <div data-react-toolbox='dialog' role='dialog' tabIndex='-1' className={className}>
+            <section role='body' className={this.props.theme.body}>
+              {this.props.title ? <h6 className={this.props.theme.title}>{this.props.title}</h6> : null}
+              {this.props.children}
+            </section>
+            {actions.length
+              ? <nav role='navigation' className={this.props.theme.navigation}>
+                  {actions}
+                </nav>
+              : null
+            }
+          </div>
+        </Overlay>
+      );
+    }
+
   }
+
+  return Dialog;
 
 }
 
+const Dialog = factory(Overlay, Button);
 export default themr(DIALOG)(Dialog);
 export { Dialog };
+
 
