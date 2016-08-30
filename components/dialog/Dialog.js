80a9a153e1f9af87481643847e0671e3a8addcd4
Felix Zapata <fzberlinches@gmail.com>
2016-08-30 10:10:32 +0200

    feat(Dialog.js): create first draft to find focusable nodes

--- components/dialog/Dialog.js
+++ components/dialog/Dialog.js
@@ -37,6 +37,64 @@ var factory = function factory(Overlay, Button) {
       type: 'normal'
     };
 
+    setTabIndex(obj, tabIdx) {
+      // Elements that can be focused even if they have [disabled] attribute.
+      const FOCUSABLE_WITH_DISABLED = [
+          'a[href]',
+          'area[href]',
+          'iframe',
+          '[tabindex]',
+          '[contentEditable=true]'
+        ];
+      // Elements that cannot be focused if they have [disabled] attribute.
+      const FOCUSABLE_WITHOUT_DISABLED = [
+          'input',
+          'select',
+          'textarea',
+          'button'
+        ];
+      // Discard elements with tabindex=-1 (makes them not focusable).
+      const selector = FOCUSABLE_WITH_DISABLED.join(':not([tabindex="-1"]),') +
+        ':not([tabindex="-1"]),' +
+        FOCUSABLE_WITHOUT_DISABLED.join(':not([disabled]):not([tabindex="-1"]),') +
+        ':not([disabled]):not([tabindex="-1"])';
+      const focusables = Array.prototype.slice.call(obj.querySelectorAll(selector));
+      const focusablesInDialog = Array.prototype.slice.call(this.refs.dialog.querySelectorAll(selector)); 
+      const len = focusables.length;
+
+      var filtered = focusables.filter(function(value){
+        return !focusablesInDialog.includes(value);
+      });
+
+      for(var i = 0; i < filtered.length; i++) {
+        filtered[i].setAttribute('tabindex', tabIdx);
+      }
+    }
+
+    componentDidMount () {
+      console.log('componentDidMount');
+    }
+
+    componentWillUpdate (nextProps) {
+
+      if (nextProps.active && !this.props.active) {
+        this.refs.dialog.focus();
+        this.setTabIndex(document.body, -1);
+      } 
+      if (!nextProps.active && this.props.active) {
+        this.setTabIndex(document.body, 0);
+      } 
+
+    }
+
+    componentDidUpdate () {
+      console.log('componentDidUpdate');
+    }
+
+    componentWillUnmount () {
+      console.log('componentWillUnmount');
+    }
+
     render () {
       const actions = this.props.actions.map((action, idx) => {
         const className = classnames(this.props.theme.button, {[action.className]: action.className});
@@ -56,7 +114,7 @@ var factory = function factory(Overlay, Button) {
           onMouseMove={this.props.onOverlayMouseMove}
           onMouseUp={this.props.onOverlayMouseUp}
         >
-          <div data-react-toolbox='dialog' role='dialog' tabIndex='-1' className={className}>
+          <div data-react-toolbox='dialog' ref='dialog' role='dialog' tabIndex='-1' className={className}>
             <section role='body' className={this.props.theme.body}>
               {this.props.title ? <h6 className={this.props.theme.title}>{this.props.title}</h6> : null}
               {this.props.children}
